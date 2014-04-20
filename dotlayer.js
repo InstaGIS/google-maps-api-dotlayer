33b34ef2cacfa776628a99592b9e0f0ae85c0840
amena <amenadiel@gmail.com>
2014-04-20 19:55:25 -0300

    fixed demo

--- dotlayer.js
+++ dotlayer.js
@@ -28,7 +28,7 @@ var THREE = THREE || {};
  * @param {Object}   options  Options passed to initialize method.
  * @param {Function} callback Callback to execute when map was updated.
  */
-google.maps.DotLayer = function(options, callback) {
+google.maps.DotLayer = function (options, callback) {
 	this.bindAll();
 	this.callback = callback;
 	var self = this;
@@ -39,7 +39,7 @@ google.maps.DotLayer = function(options, callback) {
 	 *
 	 * @return {String} The property.
 	 */
-	this.CSS_TRANSFORM = (function() {
+	this.CSS_TRANSFORM = (function () {
 		var div = document.createElement('div');
 		var props = [
 			'transform',
@@ -58,7 +58,7 @@ google.maps.DotLayer = function(options, callback) {
 
 		return props[0];
 	})();
-	require(['three'], function(REALTHREE) {
+	require(['three'], function (REALTHREE) {
 		THREE = REALTHREE;
 		self.initialize(options || {});
 
@@ -82,7 +82,7 @@ google.maps.DotLayer.prototype = new google.maps.OverlayView();
 /**
  * Bind all methods to the instance.
  */
-google.maps.DotLayer.prototype.bindAll = function() {
+google.maps.DotLayer.prototype.bindAll = function () {
 	var instance = this;
 
 	function bind(name) {
@@ -90,7 +90,7 @@ google.maps.DotLayer.prototype.bindAll = function() {
 		if (typeof method != "function") {
 			return;
 		}
-		instance[name] = function() {
+		instance[name] = function () {
 			return method.apply(instance, arguments);
 		};
 	}
@@ -107,7 +107,7 @@ google.maps.DotLayer.prototype.bindAll = function() {
  * Initialize the layer with the given options.
  * @param  {Object} options - Options
  */
-google.maps.DotLayer.prototype.initialize = function(options) {
+google.maps.DotLayer.prototype.initialize = function (options) {
 
 	this.options = options;
 	this.map = options.map;
@@ -118,7 +118,7 @@ google.maps.DotLayer.prototype.initialize = function(options) {
 		container: this.options.container
 	});
 
-	this.webgl = (function() {
+	this.webgl = (function () {
 		try {
 			var canvas = document.createElement('canvas');
 			return !!window.WebGLRenderingContext && (canvas.getContext('webgl') || canvas.getContext('experimental-webgl'));
@@ -159,7 +159,7 @@ google.maps.DotLayer.prototype.initialize = function(options) {
  * This method is called once after setMap() is called with a valid map.
  * @see https://developers.google.com/maps/documentation/javascript/reference#OverlayView
  */
-google.maps.DotLayer.prototype.onAdd = function() {
+google.maps.DotLayer.prototype.onAdd = function () {
 
 	this.map = this.getMap();
 	if (!this.map) {
@@ -174,30 +174,36 @@ google.maps.DotLayer.prototype.onAdd = function() {
 		this.draw
 	);
 
-	this.draw();
+	// We don't need to create the material more than once
+	if (!this.firstRun) {
+		this.redraw();
+	} else {
+		this.draw();
+	}
+
+
 };
 
 /**
  * This method is called once following a call to setMap(null).
  * @see https://developers.google.com/maps/documentation/javascript/reference#OverlayView
  */
-google.maps.DotLayer.prototype.onRemove = function() {
+google.maps.DotLayer.prototype.onRemove = function () {
+	var self = this;
+	self.clear(function () {
+		self.map = null;
 
-	if (!this.map) {
-		return;
-	}
+		self.canvas.parentElement.removeChild(self.canvas);
 
-	this.map = null;
+		if (self.changeHandler) {
+			google.maps.event.removeListener(self.changeHandler);
+			self.changeHandler = null;
+		}
+	});
 
-	this.canvas.parentElement.removeChild(this.canvas);
-
-	if (this.changeHandler) {
-		google.maps.event.removeListener(this.changeHandler);
-		this.changeHandler = null;
-	}
 };
 
-google.maps.DotLayer.prototype.remove = function() {
+google.maps.DotLayer.prototype.remove = function () {
 	this.canvas.parentNode.removeChild(this.canvas);
 	//this.canvas_ = null;
 };
@@ -206,12 +212,10 @@ google.maps.DotLayer.prototype.remove = function() {
 /**
  * This method is called when the layer postion needs an update.
  */
-google.maps.DotLayer.prototype.draw = function() {
-	//console.log('draw', this.map);
+google.maps.DotLayer.prototype.draw = function () {
 	if (!this.map) {
 		return;
 	}
-
 	var bounds = this.map.getBounds();
 
 	var topLeft = new google.maps.LatLng(
@@ -236,16 +240,18 @@ google.maps.DotLayer.prototype.draw = function() {
 			var self = this;
 			this.createMaterial();
 		}
-	}
 
+	}
 	this.update();
+
+
 
 };
 
 /**
  * Call this method when the layer's size changed.
  */
-google.maps.DotLayer.prototype.resize = function() {
+google.maps.DotLayer.prototype.resize = function () {
 
 	if (!this.map) {
 		return;
@@ -269,8 +275,8 @@ google.maps.DotLayer.prototype.resize = function() {
 /**
  * This method is called when the Three.js camera needs an update.
  */
-google.maps.DotLayer.prototype.update = function() {
-
+google.maps.DotLayer.prototype.update = function () {
+	if (!this.map) return;
 	var projection = this.map.getProjection(),
 		zoom, scale, offset, bounds, topLeft;
 
@@ -303,7 +309,7 @@ google.maps.DotLayer.prototype.update = function() {
 /**
  * Renders the layer deferred.
  */
-google.maps.DotLayer.prototype.render = function() {
+google.maps.DotLayer.prototype.render = function () {
 	cancelAnimationFrame(this.animationFrame);
 	this.animationFrame = requestAnimationFrame(this.deferredRender);
 };
@@ -313,7 +319,7 @@ google.maps.DotLayer.prototype.render = function() {
  * The final rendering. If you have passed a function to `options.render`
  * it will be executed here.
  */
-google.maps.DotLayer.prototype.deferredRender = function() {
+google.maps.DotLayer.prototype.deferredRender = function () {
 	if (typeof this.options.render === false) {
 		return;
 	} else if (typeof this.options.render == "function") {
@@ -327,28 +333,32 @@ google.maps.DotLayer.prototype.deferredRender = function() {
  * Shortcut method to add new geometry to the scene.
  * @param  {Geometry} geometry The Three.js geometry to add.
  */
-google.maps.DotLayer.prototype.add = function(geometry) {
+google.maps.DotLayer.prototype.add = function (geometry) {
 	this.scene.add(geometry);
 };
 
+
+
 /**
- * This method will clear the DotLayer
+ * This method will clear the DotLayer asynchronously
  */
-google.maps.DotLayer.prototype.clear = function() {
-	for (i = this.scene.children.length - 1; i >= 0; i--) {
-		obj = this.scene.children[i];
+google.maps.DotLayer.prototype.clear = function (callback) {
+	if (this.scene.children.length) {
+		obj = this.scene.children.pop();
 		if (obj.is_ob) {
 			this.scene.originalparticles = obj;
 			this.scene.remove(obj);
 		}
 	}
 	this.update();
+	if (callback) callback();
 };
 
 /**
  * This method will redraw the DotLayer if you previously cleared it
  */
-google.maps.DotLayer.prototype.redraw = function() {
+google.maps.DotLayer.prototype.redraw = function () {
+	this.scene.originalparticles = this.scene.originalparticles || this.scene.children[0];
 	this.scene.add(this.scene.originalparticles);
 	this.update();
 };
@@ -358,7 +368,7 @@ google.maps.DotLayer.prototype.redraw = function() {
  * @param  {google.maps.LatLng} latLng - The LatLng to convert.
  * @return {THREE.Vector3} The resulting vertex.
  */
-google.maps.DotLayer.prototype.fromLatLngToVertex = function(latLng) {
+google.maps.DotLayer.prototype.fromLatLngToVertex = function (latLng) {
 	var projection = this.map.getProjection(),
 		point = projection.fromLatLngToPoint(latLng),
 		vertex = new THREE.Vector3();
@@ -369,7 +379,7 @@ google.maps.DotLayer.prototype.fromLatLngToVertex = function(latLng) {
 	return vertex;
 };
 
-google.maps.DotLayer.prototype.generateSprite = function(dotcolor, size, renderer) {
+google.maps.DotLayer.prototype.generateSprite = function (dotcolor, size, renderer) {
 	var canvas = document.createElement('canvas'),
 		context = canvas.getContext('2d'),
 		transform = {},
@@ -425,7 +435,7 @@ google.maps.DotLayer.prototype.generateSprite = function(dotcolor, size, rendere
 	return canvas;
 };
 
-google.maps.DotLayer.prototype.hextodecColor = function(hex) {
+google.maps.DotLayer.prototype.hextodecColor = function (hex) {
 	if (!hex) return 'rgba(100,250,50,0.99)';
 	var deccolor = [];
 	deccolor.r = parseInt(hex.substring(0, 2), 16);
@@ -437,11 +447,11 @@ google.maps.DotLayer.prototype.hextodecColor = function(hex) {
 
 
 
-google.maps.DotLayer.prototype.getDataSet = function() {
+google.maps.DotLayer.prototype.getDataSet = function () {
 	return this.DataSet;
 };
 
-google.maps.DotLayer.prototype.setDataSet = function(DataSet) {
+google.maps.DotLayer.prototype.setDataSet = function (DataSet) {
 	this.DataSet = DataSet;
 	// we define default value for needed properties
 	DataSet.id_dataset = DataSet.id_dataset || 'dataset1337';
@@ -450,9 +460,9 @@ google.maps.DotLayer.prototype.setDataSet = function(DataSet) {
 	DataSet.visualizations = DataSet.visualizations || {};
 	DataSet.visualizations.dotOptions = DataSet.visualizations.dotOptions || {};
 	DataSet.color = DataSet.color || '#F00';
-	DataSet.loadPoints = DataSet.loadPoints || function(callback) {
+	DataSet.loadPoints = DataSet.loadPoints || function (callback) {
 		var origin = [];
-		this.forEach(function(element) {
+		this.forEach(function (element) {
 			var position;
 			// Let's check if the element is a position or at least has a position attribute.
 			if (element.constructor == google.maps.LatLng) {
@@ -473,7 +483,7 @@ google.maps.DotLayer.prototype.setDataSet = function(DataSet) {
 
 
 
-google.maps.DotLayer.prototype.createMaterial = function(origin) {
+google.maps.DotLayer.prototype.createMaterial = function (origin) {
 
 	//console.log('Origin is', origin);
 	var self = this,
@@ -482,7 +492,7 @@ google.maps.DotLayer.prototype.createMaterial = function(origin) {
 		particleoptions;
 
 
-	DataSet.loadPoints(function(origin) {
+	DataSet.loadPoints(function (origin) {
 		self.origin = origin;
 
 		if (self.renderertype == 'WebGL') {
@@ -503,7 +513,7 @@ google.maps.DotLayer.prototype.createMaterial = function(origin) {
 
 			self.material = new THREE.ParticleBasicMaterial(particleoptions);
 
-			self.origin.forEach(function(element, index) {
+			self.origin.forEach(function (element, index) {
 				var vertex = self.fromLatLngToVertex(element);
 				vertex.y = vertex.y; //- 0.0001;
 				vertex.x = vertex.x; //- 0.00012;
@@ -525,9 +535,8 @@ google.maps.DotLayer.prototype.createMaterial = function(origin) {
 
 			particles = new THREE.Object3D();
 			self.material = new THREE.SpriteMaterial(particleoptions);
-			var projection = this.getProjection();
 
-			self.origin.forEach(function(element, index) {
+			self.origin.forEach(function (element, index) {
 				geometry = new THREE.Sprite(self.material);
 				geometry.position = self.fromLatLngToVertex(element);
 				geometry.position.y = geometry.position.y - 0.0001;
@@ -560,7 +569,7 @@ google.maps.DotLayer.prototype.createMaterial = function(origin) {
 
 
 
-google.maps.DotLayer.prototype.addDotControls = function() {
+google.maps.DotLayer.prototype.addDotControls = function () {
 	var self = this,
 		material = self.material,
 		container = self.options.container || 'body',
@@ -583,7 +592,7 @@ google.maps.DotLayer.prototype.addDotControls = function() {
 		self.render();
 	}
 
-	require(['jquery', 'dat.gui'], function(jQuery, GUI) {
+	require(['jquery', 'dat.gui'], function (jQuery, GUI) {
 
 		jQuery('.dot', container).remove();
 
@@ -602,12 +611,12 @@ google.maps.DotLayer.prototype.addDotControls = function() {
 
 			jQuery('.folder .dg li.title', thegui).append('<span class="pull-right"></span>');
 
-			dotcontrols.addColor(material, 'dotcolor').onChange(function(value) {
+			dotcontrols.addColor(material, 'dotcolor').onChange(function (value) {
 				DataSet.dot.texture = new THREE.Texture(self.generateSprite(material.dotcolor, material.size, self.renderertype));
 				DataSet.dot.texture.needsUpdate = true;
 				material.map = self.texture;
 				updateColor(value);
-			}).onFinishChange(function(value) {
+			}).onFinishChange(function (value) {
 				google.maps.event.trigger(self.getMap(), "dataset_changed", {
 					dataset: self.getDataSet(),
 					from: 'Dot Controls'
@@ -619,18 +628,18 @@ google.maps.DotLayer.prototype.addDotControls = function() {
 
 
 			if (self.renderertype == 'WebGL') {
-				dotcontrols.add(material, 'size', 2, 200).onChange(updateSize).onFinishChange(function(value) {
+				dotcontrols.add(material, 'size', 2, 200).onChange(updateSize).onFinishChange(function (value) {
 					google.maps.event.trigger(self.getMap(), "dataset_changed", {
 						dataset: self.getDataSet(),
 						from: 'Dot Controls'
 					});
 				});
 			} else {
-				dotcontrols.add(material, 'size', 2, 200).onChange(function(newValue) {
+				dotcontrols.add(material, 'size', 2, 200).onChange(function (newValue) {
 					DataSet.dot.texture = new THREE.Texture(self.generateSprite(material.dotcolor, material.size, self.renderertype));
 					material.map = DataSet.dot.texture;
 					updateSize(newValue);
-				}).onFinishChange(function(value) {
+				}).onFinishChange(function (value) {
 					google.maps.event.trigger(self.getMap(), "dataset_changed", {
 						dataset: self.getDataSet(),
 						from: 'Dot Controls'
@@ -638,7 +647,7 @@ google.maps.DotLayer.prototype.addDotControls = function() {
 				});
 			}
 
-			dotcontrols.add(material, 'opacity', 0.1, 1).listen().onChange(updateOpacity).onFinishChange(function(value) {
+			dotcontrols.add(material, 'opacity', 0.1, 1).listen().onChange(updateOpacity).onFinishChange(function (value) {
 				google.maps.event.trigger(self.getMap(), "dataset_changed", {
 					dataset: self.getDataSet(),
 					from: 'Dot Controls'
